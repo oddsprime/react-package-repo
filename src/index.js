c3b7fea13b8bb9dcc3140867cc8db469ac00cd8f
oddsprime <oddsprime@gmail.com>
2024-07-19 00:17:19 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import MyComponent from './MyComponent';
+import React from 'react';
+
+const MyComponent = () => {
+  return <div>Hello, this is my component!</div>;
+};
 
 export default MyComponent;
